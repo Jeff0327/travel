b5986c2a7985ee15480109288a85310fa40cd04b
jeff0327 <jeff0327@gmail.com>
2024-07-08 17:33:54 +0900

    prismaDB

--- src/libs/prismaDB.js
+++ src/libs/prismaDB.js
@@ -1,6 +1,17 @@
-// libs/prismaDB.js
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
-export { prisma };
+const globalForPrisma = global;
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
